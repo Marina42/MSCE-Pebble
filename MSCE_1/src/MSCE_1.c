a9a922a30dba01a524f2455380b67b9ecdb0f3e8
David da Silva <daviddasilvacontin@me.com>
2014-03-27 12:07:23 +0100

    Removed unused functions, declared some defines, global GPoint.

--- MSCE_1/src/MSCE_1.c
+++ MSCE_1/src/MSCE_1.c
@@ -1,58 +1,39 @@
 #include <pebble.h>
 
-static Window *window;
+#define SCREEN_WIDTH 144
+#define SCREEN_HEIGHT 151
+#define FPS 60
+#define INTERVAL (1000/FPS)
 
-int ball_x = 25;
-int ball_y = 25;
+static Window *window;
+struct Layer *window_layer;
+
+GPoint ball;
 
 int hor_dir = 1;
 int ver_dir = 1;
 
-struct Layer *window_layer;
-
-
+int ball_radius = 5;
 
 void render (void* callback_data){
   layer_mark_dirty(window_layer);
-  app_timer_register (33, render, NULL);
-}
-
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-
-}
-
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  app_timer_register (INTERVAL, render, NULL);
 }
 
 static void my_layer_draw(Layer *layer, GContext *ctx) {
-    GPoint ball;
-    ball.x = ball_x;
-    ball.y = ball_y;
 
     graphics_context_set_fill_color(ctx, GColorWhite);
-    graphics_fill_circle(ctx, ball, 5);
+    graphics_fill_circle(ctx, ball, ball_radius);
 
-    ball_x += hor_dir;
-    ball_y += ver_dir;
+    if (ball.x >= SCREEN_WIDTH-ball_radius || ball.x <= ball_radius) hor_dir *= -1;
+    if (ball.y >= SCREEN_HEIGHT-ball_radius || ball.y <= ball_radius) ver_dir *= -1;
 
-    if (ball_x >= 139 || ball_x <= 5) hor_dir *= -1;
-    if (ball_y >= 146 || ball_y <= 5) ver_dir *= -1; 
-
-    //GPoint ball;
-    ball.x = ball_x;
-    ball.y = ball_y;
+    ball.x += hor_dir;
+    ball.y += ver_dir;
 
     graphics_context_set_fill_color(ctx, GColorBlack);
-    graphics_fill_circle(ctx, ball, 5);
+    graphics_fill_circle(ctx, ball, ball_radius);
+
 }
 
 static void window_load(Window *window) {
@@ -60,13 +41,14 @@ static void window_load(Window *window) {
 }
 
 static void window_unload(Window *window) {
+  
 }
 
 static void init(void) {
+
   window = window_create();
   window_layer = window_get_root_layer (window);
   layer_set_update_proc(window_layer, my_layer_draw);
-  window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
@@ -81,11 +63,15 @@ static void deinit(void) {
 }
 
 int main(void) {
+
+  ball.x = 25;
+  ball.y = 25;
+
   init();
   
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
-  app_timer_register(33, render, NULL);
+  app_timer_register(INTERVAL, render, NULL);
   app_event_loop();
   deinit();
 }
